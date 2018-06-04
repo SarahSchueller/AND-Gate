fc812f38b054515fc45cae46829ab8bf2b3c0620
SarahSchueller <37308347+SarahSchueller@users.noreply.github.com>
2018-06-04 09:40:34 +0200

    Update LogFuncTest.py
    new tests

--- LogFuncTest.py
+++ LogFuncTest.py
@@ -6,108 +6,144 @@ from LogFunc import NAndGate
 class AndGateTest(unittest.TestCase):
 
     def testcase_00(self):
-        a = AndGate()
-        self.assertFalse(a.Input0, 'Class NAndGate: Testcase 0 failed.')
-        self.assertFalse(a.Input1, 'Class NAndGate: Testcase 0 failed.')
-        self.assertFalse(a.Output, 'Class NAndGate: Testcase 0 failed.')
+        a = AndGate(4)
+        for x in range(4):
+            self.assertFalse(a.Input[x], 'Class AndGate: Testcase 0 failed.')
+        self.assertFalse(a.Output, 'Class AndGate: Testcase 0 failed.')
 
     def testcase_01(self):
-        a = AndGate()
-        a.Input0 = False
-        a.Input1 = False
-        a.execute()
-        self.assertFalse(a.Output, 'Class AndGate: Testcase 1 failed.')
+        for y in range(4):
+            a = AndGate(4)
+            for x in range(1):
+                a.setInput(x + y, 1)
+            a.execute()
+            self.assertFalse(a.Output, 'Class AndGate: Testcase 1_%s failed.'%(y))
 
     def testcase_02(self):
-        a = AndGate()
-        a.Input0 = True
-        a.Input1 = False
+        for y in range(3):
+            a = AndGate(4)
+            for x in range(2):
+                a.setInput(x + y, 1)
+            a.execute()
+            self.assertFalse(a.Output, 'Class AndGate: Testcase 2_%s failed.'%(y))
+
+    def testcase_02_3(self):
+        a = AndGate(4)
+        a.setInput(3, 1)
+        a.setInput(0, 1)
         a.execute()
-        self.assertFalse(a.Output, 'Class AndGate: Testcase 2 failed.')
+        self.assertFalse(a.Output, 'Class AndGate: Testcase 2_3 failed.')
     
     def testcase_03(self):
-        a = AndGate()
-        a.Input0 = False
-        a.Input1 = True
-        a.execute()
-        self.assertFalse(a.Output, 'Class AndGate: Testcase 3 failed.')
+        for y in range(4):
+            a = AndGate(4)
+            for z in range(4):
+                a.setInput(z , 1)
+            for x in range(1):
+                a.setInput(x + y, 0)
+            a.execute()
+            self.assertFalse(a.Output, 'Class AndGate: Testcase 3_%s failed.'%(y))
     
     def testcase_04(self):
-        a = AndGate()
-        a.Input0 = True
-        a.Input1 = True
+        a = AndGate(4)
+        for x in range(4):
+            a.setInput(x, 1)
         a.execute()
         self.assertTrue(a.Output, 'Class AndGate: Testcase 4 failed.')
 
 class OrGateTest(unittest.TestCase):
 
     def testcase_00(self):
-        a = OrGate()
-        self.assertFalse(a.Input0, 'Class NAndGate: Testcase 0 failed.')
-        self.assertFalse(a.Input1, 'Class NAndGate: Testcase 0 failed.')
-        self.assertFalse(a.Output, 'Class NAndGate: Testcase 0 failed.')
+        a = OrGate(4)
+        for x in range(4):
+            self.assertFalse(a.Input[x], 'Class OrGate: Testcase 0 failed.')
+        self.assertFalse(a.Output, 'Class OrGate: Testcase 0 failed.')
 
     def testcase_01(self):
-        a = OrGate()
-        a.Input0 = False
-        a.Input1 = False
-        a.execute()
-        self.assertFalse(a.Output, 'Class OrGate: Testcase 1 failed.')
+        for y in range(4):
+            a = OrGate(4)
+            for x in range(1):
+                a.setInput(x + y, 1)
+            a.execute()
+            self.assertTrue(a.Output, 'Class Orate: Testcase 1_%s failed.'%(y))
 
     def testcase_02(self):
-        a = OrGate()
-        a.Input0 = True
-        a.Input1 = False
+        for y in range(3):
+            a = OrGate(4)
+            for x in range(2):
+                a.setInput(x + y, 1)
+            a.execute()
+            self.assertTrue(a.Output, 'Class OrGate: Testcase 2_%s failed.'%(y))
+
+    def testcase_02_3(self):
+        a = OrGate(4)
+        a.setInput(3, 1)
+        a.setInput(0, 1)
         a.execute()
-        self.assertTrue(a.Output, 'Class OrGate: Testcase 2 failed.')
+        self.assertTrue(a.Output, 'Class OrGate: Testcase 2_3 failed.')
     
     def testcase_03(self):
-        a = OrGate()
-        a.Input0 = False
-        a.Input1 = True
-        a.execute()
-        self.assertTrue(a.Output, 'Class OrGate: Testcase 3 failed.')
+        for y in range(4):
+            a = OrGate(4)
+            for z in range(4):
+                a.setInput(z , 1)
+            for x in range(1):
+                a.setInput(x + y, 0)
+            a.execute()
+            self.assertTrue(a.Output, 'Class OrGate: Testcase 3_%s failed.'%(y))
     
     def testcase_04(self):
-        a = OrGate()
-        a.Input0 = True
-        a.Input1 = True
+        a = OrGate(4)
+        for x in range(4):
+            a.setInput(x, 1)
         a.execute()
         self.assertTrue(a.Output, 'Class OrGate: Testcase 4 failed.')
 
 class NAndGateTest(unittest.TestCase):
 
     def testcase_00(self):
-        a = NAndGate()
-        self.assertFalse(a.Input0, 'Class NAndGate: Testcase 0 failed.')
-        self.assertFalse(a.Input1, 'Class NAndGate: Testcase 0 failed.')
+        a = NAndGate(4)
+        for x in range(4):
+            self.assertFalse(a.Input[x], 'Class NAndGate: Testcase 0 failed.')
         self.assertTrue(a.Output, 'Class NAndGate: Testcase 0 failed.')
 
     def testcase_01(self):
-        a = NAndGate()
-        a.Input0 = False
-        a.Input1 = False
-        a.execute()
-        self.assertTrue(a.Output, 'Class NAndGate: Testcase 1 failed.')
+        for y in range(4):
+            a = NAndGate(4)
+            for x in range(1):
+                a.setInput(x + y, 1)
+            a.execute()
+            self.assertTrue(a.Output, 'Class NAndGate: Testcase 1_%s failed.'%(y))
 
     def testcase_02(self):
-        a = NAndGate()
-        a.Input0 = True
-        a.Input1 = False
+        for y in range(3):
+            a = NAndGate(4)
+            for x in range(2):
+                a.setInput(x + y, 1)
+            a.execute()
+            self.assertTrue(a.Output, 'Class NAndGate: Testcase 2_%s failed.'%(y))
+
+    def testcase_02_3(self):
+        a = NAndGate(4)
+        a.setInput(3, 1)
+        a.setInput(0, 1)
         a.execute()
-        self.assertTrue(a.Output, 'Class NAndGate: Testcase 2 failed.')
+        self.assertTrue(a.Output, 'Class NAndGate: Testcase 2_3 failed.')
     
     def testcase_03(self):
-        a = NAndGate()
-        a.Input0 = False
-        a.Input1 = True
-        a.execute()
-        self.assertTrue(a.Output, 'Class NAndGate: Testcase 3 failed.')
+        for y in range(4):
+            a = NAndGate(4)
+            for z in range(4):
+                a.setInput(z , 1)
+            for x in range(1):
+                a.setInput(x + y, 0)
+            a.execute()
+            self.assertTrue(a.Output, 'Class NAndGate: Testcase 3_%s failed.'%(y))
     
     def testcase_04(self):
-        a = NAndGate()
-        a.Input0 = True
-        a.Input1 = True
+        a = NAndGate(4)
+        for x in range(4):
+            a.setInput(x, 1)
         a.execute()
         self.assertFalse(a.Output, 'Class NAndGate: Testcase 4 failed.')
 
